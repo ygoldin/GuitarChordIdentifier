cdd1bab2ef3b16460f047b5e94b3a66415440b9a
Yael Goldin <ygoldin@cs.washington.edu>
2021-02-21 13:58:32 -0800

    try to change eslint

---
+++ src/sdk/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    settings: {
+        react: {
+            createClass: 'createReactClass',
+            pragma: 'React',
+            version: 'detect',
+        },
+        linkComponents: [
+            // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+            'Hyperlink',
+            {name: 'Link', linkAttribute: 'to'}
+        ]
+    },
+    rules: {
+    }
+}
